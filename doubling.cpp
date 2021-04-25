482b816f504237b1d7a19124c9cfd218dec0d30d
kaito-cpro <blue7-go0112@docomo.ne.jp>
2021-04-26 05:35:41 +0900

    update

--- doubling.cpp
+++ doubling.cpp
@@ -5,13 +5,13 @@ using ll = long long;
 
 int main() {
     ll N, X, M;  cin >> N >> X >> M;
-    const int MAX_D = 65;  // log(max(N))
+    const int MAX_D = 62;  // log(max(N))
 
     vector<vector<ll>> next(MAX_D + 1, vector<ll>(M, -1)), sum(MAX_D + 1, vector<ll>(M));
     // next[k][v] := 値 v の 2^k ステップ後の値
     // sum[k][v]  := 値 v から 2^k 項分の総和(つまり 1 ステップ後の値から 2^k - 1 ステップ後の値までの総和)
     for (ll v = 0; v < M; ++v) {
-        next[0][v] = v * v % M;
+        next[0][v] = v * v % M;  // 写像の定義
         sum[0][v] = v;
     }
     for (int k = 0; k < MAX_D; ++k) {
